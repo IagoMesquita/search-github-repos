212deb8b998364175249e8fe1bdcb191aed41f7f
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-07-04 18:12:11 -0300

    feat: clean name in input of repos

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,17 @@
 import './Header.css';
+import { useContext } from 'react';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import logoGithub from './images/icons8-github.svg';
+import MyContext from '../../context/MyContext';
 function Header() {
   const  history  = useHistory();
   const { pathname } = history.location;
 
+  const { setNameRepo } = useContext(MyContext);
+
   const returningForHome = () => {
     localStorage.clear();
+    setNameRepo("");
     history.push('/')
   }
 
