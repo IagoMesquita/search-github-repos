30d1d20cbf3bd3e4aaf1d8bd1793e8e25cc5a02d
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-07-04 18:19:54 -0300

    refactor: index

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-// import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import ReposProvider from './context/ReposProvider';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <ReposProvider>
-      <App />
-    </ReposProvider>
-   </BrowserRouter>,
-);
-
-// ReactDOM.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
 //   <BrowserRouter>
 //     <ReposProvider>
 //       <App />
 //     </ReposProvider>
-//   </BrowserRouter>,
-  
-//   document.getElementById('root')
+//    </BrowserRouter>,
 // );
+
+ReactDOM.render(
+  <BrowserRouter>
+    <ReposProvider>
+      <App />
+    </ReposProvider>
+  </BrowserRouter>,
+  
+  document.getElementById('root')
+);
 
